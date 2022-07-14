d81177c8b43d09586ce6df1bd73be0b26b0f5eb9
Robert Schroll <robert@thedataincubator.com>
2022-07-14 17:51:21 +0000

    Create a database.py file to query wells database
    Right now, it's taking the connection string as an argument.  We
    should find a better way to manage this.

---
+++ app/database.py
@@ -0,0 +1,22 @@
+import sys
+from sqlalchemy import create_engine
+
+def query_db(depth, gradient, conn_str):
+    engine = create_engine(conn_str)
+
+    query = f"""
+    SELECT latitude, longitude, depth, gradient
+    FROM wells
+    WHERE depth > {depth} AND gradient > {gradient};
+    """
+
+    with engine.connect() as conn:
+        results = conn.execute(query).fetchall()
+
+    return results
+
+if __name__ == '__main__':
+    depth = sys.argv[1]
+    gradient = sys.argv[2]
+    conn_str = sys.argv[3]
+    print(query_db(depth, gradient, conn_str))
